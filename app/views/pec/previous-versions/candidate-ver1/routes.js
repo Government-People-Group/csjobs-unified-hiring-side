4ec9babaea5b4057ea1c8d0a8564601abf05bab5
ismail-co <ismail.ismail@cabinetoffice.gov.uk>
2023-08-22 17:50:40 +0100

    fixes

--- app/views/pec/previous-versions/candidate-ver1/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const radioButtonRedirect = require('radio-button-redirect')
-router.use(radioButtonRedirect)
-
-// Job library entry
-
-
-module.exports = router
